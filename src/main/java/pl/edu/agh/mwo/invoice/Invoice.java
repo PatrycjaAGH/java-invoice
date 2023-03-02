7ae7bac99e7c64012a88901a7a75697d956f4267
Patrycja <patrycja.barbara.szczepanska@gmail.com>
2023-03-02 21:18:11 +0100

    zadanie

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,6 +1,7 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
@@ -8,25 +9,46 @@ import pl.edu.agh.mwo.invoice.product.Product;
 public class Invoice {
     //private Collection<Product> products;
 
-    private Map<Product, Integer> products;
+    private Map<Product, Integer> products=new HashMap<>();
 
     public void addProduct(Product product) {
-        // TODO: implement
+        if (product == null) throw new IllegalArgumentException();
+        this.products.put(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
+        if (quantity <= 0) throw new IllegalArgumentException();
+        if (product == null) throw new IllegalArgumentException();
         this.products.put(product, quantity);
     }
 
     public BigDecimal getNetTotal() {
-        return BigDecimal.ZERO;
+        BigDecimal result = BigDecimal.ZERO;
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            int quantity = entry.getValue();
+            Product product = entry.getKey();
+
+            result = result.add(product.getPrice().multiply(BigDecimal.valueOf(quantity)));
+        }
+        return result;
     }
 
     public BigDecimal getTax() {
-        return BigDecimal.ZERO;
+        BigDecimal result = BigDecimal.ZERO;
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            int quantity = entry.getValue();
+            Product product = entry.getKey();
+            BigDecimal quantityBD = BigDecimal.valueOf(quantity);
+            BigDecimal price = product.getPrice();
+            BigDecimal taxPer = product.getTaxPercent();
+            BigDecimal tax = price.multiply(taxPer);
+            BigDecimal totalTax = tax.multiply(quantityBD);
+            result = result.add(totalTax);
+        }
+        return result;
     }
 
     public BigDecimal getTotal() {
-        return BigDecimal.ZERO;
+        return getNetTotal().add(getTax());
     }
 }
